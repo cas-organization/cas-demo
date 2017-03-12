231b162386663f6e691d97d88c42e92407658607
Brun <Matthias.Brun@zuehlke.com>
2017-03-12 15:23:20 +0100

    Initial commit

---
+++ src/test/java/ch/fhnw/recruiting/RecruitingApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.fhnw.recruiting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruitingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
