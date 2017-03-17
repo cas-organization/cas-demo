00f213741d51b3f7979f4f2b0adcf888e81a863b
Matthias Brun <matthias.brun@gmail.com>
2017-03-17 09:06:44 +0100

    Remove logging

--- src/main/java/ch/fhnw/recruiting/RecruitingApplication.java
+++ src/main/java/ch/fhnw/recruiting/RecruitingApplication.java
@@ -5,9 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
 
 @SpringBootApplication
@@ -16,20 +13,8 @@ public class RecruitingApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(RecruitingApplication.class);
 
-    private static int PORT;
-
     public static void main(String[] args) {
         SpringApplication.run(RecruitingApplication.class, args);
-        logger.info("You can reach the web app under: http://localhost:{}/", PORT);
     }
 
-    @Component
-    public static class ServletContainerListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
-
-        @Override
-        public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-            PORT = event.getEmbeddedServletContainer().getPort();
-        }
-
-    }
 }
